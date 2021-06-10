74915b8849e3ac431c55c4d133accbd9cf0a5e31
Joseph Lio <josephlio@hotmail.com>
2021-06-10 01:16:41 -0700

    Complete countUniqueValues two pointers

---
+++ countUniqueValues.js
@@ -0,0 +1,18 @@
+const countUniqueValues = (array) => {
+  let first = 0;
+  let second = 1;
+  let count = 1;
+
+  while (second < array.length - 1) {
+    while (array[second] === array[first]) {
+      second++;
+    }
+    if (second < array.length) {
+      count++;
+    }
+    first = second;
+  }
+  return count;
+}
+
+console.log(countUniqueValues([1, 1, 1]));
