918c462b563f0ceadb1a5eb7da4e62c2e2d49b1d
Joseph Lio <josephlio@hotmail.com>
2021-06-24 12:59:09 -0700

    Complete bubblesort.js

--- bubblesort.js
+++ bubblesort.js
@@ -1,15 +1,11 @@
 const bubbleSort = (array) => {
 
-  let swapped = true;
-
-  while (swapped) {
-    swapped = false;
-    for (let i = 0; i < array.length - 1; i++) {
+  for (let j = array.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
       if (array[i] > array[i + 1]) {
         let temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
-        swapped = true;
       }
     }
   }
