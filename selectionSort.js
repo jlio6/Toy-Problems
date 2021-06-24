08b4e17403194bda3b4102de1807c6f478eeb57b
Joseph Lio <josephlio@hotmail.com>
2021-06-24 14:41:16 -0700

    complete selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,23 @@
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = array[i];
+    let minIdx = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < min) {
+        min = array[j];
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      let temp = array[i];
+      array[i] = array[minIdx];
+      array[minIdx] = temp;
+    }
+  }
+  return array;
+}
+// i: 0
+// j: 2
+// min: 1
+// minIdx:
+console.log(selectionSort([1, 5, 7, 3, 63, 2, 6, 8, 4, 345 , 2]));
