fc19f277e76996b0054551f1806c00ddf5073cb2
Joseph Lio <josephlio@hotmail.com>
2021-07-27 00:23:33 -0700

    complete removeNthFromEnd with one pass with two pointers leetcode

--- removeNthNode.js
+++ removeNthNode.js
@@ -5,57 +5,29 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-/**
+/*
  * @param {ListNode} head
  * @param {number} n
  * @return {ListNode}
-
- From head, go all the way to the tail
-    while node.next
-        node = node.next
-
- Reverse linked list
- start from head and go next until n - 1
- head.next = head.next.next
- Reverse linked list again and return
-
  */
 
  var removeNthFromEnd = function(head, n) {
-
-  const reverseLinkedList = (head) => {
-      if (!head) {
-          return null;
-      }
-      if (!head.next) {
-          return head;
-      }
-      let prev = null;
-      let current = head;
-      let next = current.next;
-
-      while (next) {
-          next = current.next;
-          current.next = prev;
-          prev = current;
-          current = next;
-      }
-      return prev;
-  }
-
-  head = reverseLinkedList(head);
-  console.log(head);
+  let dummy = head;
   let headCopy = head;
-  if (n === 1) {
+  for (let i = 1; i <= n; i++) {
       head = head.next;
-      headCopy = head;
-  } else {
-      for (let i = 1; i < n - 1; i++) {
-          head = head.next;
-      }
-      head.next = head.next.next;
   }
+  let tailCopy = head;
+  if (tailCopy) {
+      while (tailCopy.next) {
+          tailCopy = tailCopy.next;
+          headCopy = headCopy.next;
+      }
+  } else {
+      return dummy.next;
+  }
+  headCopy.next = headCopy.next.next;
 
-  return reverseLinkedList(headCopy);
+  return dummy;
 
 };
