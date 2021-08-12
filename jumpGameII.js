753d8c3293aa7c41ca3351db75366c6f479b4cb4
Joseph Lio <josephlio@hotmail.com>
2021-08-12 15:05:15 -0700

    complete jumpgameII with dp array

--- jumpGameII.js
+++ jumpGameII.js
@@ -3,45 +3,48 @@
  * @return {number}
  */
  var jump = function(nums) {
-  //     // [2, 2, 2, 4]
+      // [2, 2, 2, 4]
 
-  //     // initialize min at INFINITY  // 1
-  //     let min = Infinity;
+      // initialize min at INFINITY  // 1
+      let min = Infinity;
+      let dp = new Array(nums.length).fill(-1);
 
-  //     // helper takes start position, counter
-  //     const helper = (position, counter) => {
-  //         if (position === nums.length - 1) {
-  //             min = Math.min(min, counter);
-  //             return;
-  //         }
-  //         for (let i = 0; i < nums[position]; i++) {
-  //             counter++;
-  //             helper(position + i + 1, counter);
-  //             counter--;
-  //         }
-  //     }
-  //         // if start position is equal to length - 1
-  //             // if counter is less than min, set min equal to counter
-  //             // return
-  //         // for loop from 0 to num at start position
-  //             // counter++
-  //             // rerun function with (start + i + 1)
-  //             // counter--
+      // helper takes start position, counter
+      const helper = (position) => {
+          if (position >= nums.length - 1) {
+              return 0;
+          }
+          if (dp[position] !== -1) {
+              return dp[position];
+          }
 
-  //     // run helper function (0 start position, 0)
-  //     helper(0, 0);
-  //     return min;
-
-      let jumps = 0;
-      let curEnd = 0;
-      let curFarthest = 0;
-
-    for (let i = 0; i < nums.length - 1; i++) {
-      curFarthest = Math.max(curFarthest, i + nums[i]);
-      if (i === curEnd) {
-        jumps++;
-        curEnd = curFarthest;
+          for (let i = 1; i <= nums[position]; i++) {
+              min = Math.min(min, 1 + helper(position + i));
+          }
+          dp[position] = min;
+          return min;
       }
-    }
-    return jumps;
+          // if start position is equal to length - 1
+              // if counter is less than min, set min equal to counter
+              // return
+          // for loop from 0 to num at start position
+              // counter++
+              // rerun function with (start + i + 1)
+              // counter--
+
+      // run helper function (0 start position, 0)
+      return helper(0);
+
+    //   let jumps = 0;
+    //   let curEnd = 0;
+    //   let curFarthest = 0;
+
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //   curFarthest = Math.max(curFarthest, i + nums[i]);
+    //   if (i === curEnd) {
+    //     jumps++;
+    //     curEnd = curFarthest;
+    //   }
+    // }
+    // return jumps;
   };
