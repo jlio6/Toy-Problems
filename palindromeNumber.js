32c4306ca1e3ab2a4ca644bd2d56bde99e5805c6
Joseph Lio <josephlio@hotmail.com>
2021-08-19 11:47:16 -0700

    complete palindromeNumber leetcode 9 using digits

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -3,21 +3,43 @@
  * @return {boolean}
  */
 
-// 121
+// 1001
 
 var isPalindrome = function(x) {
   if (x < 0) {
       return false;
   }
-  let temp = x.toString().split('');
-  if (temp.length <= 1) {
-      return true;
+  let temp = x; // 0
+  let numDigits = 0; // 3
+  let ones = 1;
+  while (temp) {
+      numDigits++;
+      temp = Math.floor(temp / 10);
   }
-  let firstNum = temp[0];
-  let lastNum = temp[temp.length - 1];
-  if (firstNum === lastNum) {
-      let remainder = Number(temp.slice(1, -1).join(''));
-      return isPalindrome(remainder);
+
+  while (true) {
+      let first = Math.floor(x / Math.pow(10, numDigits - 1)) % 10; // 1
+      let last = x % 10; // 1
+
+      if (first === last) {
+          numDigits = numDigits - 2;
+          if (numDigits <= ones) {
+              return true;
+          }
+          x = Math.floor(x / 10); // 100
+      } else {
+          return false;
+      }
   }
+  // let temp = x.toString().split('');
+  // if (temp.length <= 1) {
+  //     return true;
+  // }
+  // let firstNum = temp[0];
+  // let lastNum = temp[temp.length - 1];
+  // if (firstNum === lastNum) {
+  //     let remainder = Number(temp.slice(1, -1).join(''));
+  //     return isPalindrome(remainder);
+  // }
   return false;
 };
