ad89bf8e61c82eee3a89acaa7dffad9319b22aaf
Joseph Lio <josephlio@hotmail.com>
2021-08-19 16:40:55 -0700

    Complete palindromeNumber leetcode 9 with single loop reverse number

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -9,28 +9,38 @@ var isPalindrome = function(x) {
   if (x < 0) {
       return false;
   }
-  let temp = x; // 0
-  let numDigits = 0; // 3
-  let ones = 1;
-  while (temp) {
-      numDigits++;
-      temp = Math.floor(temp / 10);
+  if (x % 10 === 0 && x !== 0) {
+      return false;
   }
-
-  while (true) {
-      let first = Math.floor(x / Math.pow(10, numDigits - 1)) % 10; // 1
-      let last = x % 10; // 1
-
-      if (first === last) {
-          numDigits = numDigits - 2;
-          if (numDigits <= ones) {
-              return true;
-          }
-          x = Math.floor(x / 10); // 100
-      } else {
-          return false;
-      }
+  let reversed = 0;
+  let copyX = x;
+  while (copyX) {
+      reversed = reversed * 10 + copyX % 10;
+      copyX = Math.floor(copyX / 10);
   }
+  return reversed === x;
+//     let temp = x; // 0
+//     let numDigits = 0; // 3
+//     let ones = 1;
+//     while (temp) {
+//         numDigits++;
+//         temp = Math.floor(temp / 10);
+//     }
+
+//     while (true) {
+//         let first = Math.floor(x / Math.pow(10, numDigits - 1)) % 10; // 1
+//         let last = x % 10; // 1
+
+//         if (first === last) {
+//             numDigits = numDigits - 2;
+//             if (numDigits <= ones) {
+//                 return true;
+//             }
+//             x = Math.floor(x / 10); // 100
+//         } else {
+//             return false;
+//         }
+//     }
   // let temp = x.toString().split('');
   // if (temp.length <= 1) {
   //     return true;
